a8c8fa6dd4369d28d5d91478f17b196873740c79
TheOnlySmartBoy <o2jose43@gmail.com>
2019-05-31 14:49:18 +0300

    Initial commit

---
+++ src/Auth/LoginController.java
@@ -0,0 +1,22 @@
+package Auth;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.Initializable;
+/**
+ * FXML Controller class
+ * @author Theonlysmartboy <Theonlysmartboy@github.com>
+ */
+public class LoginController implements Initializable {
+
+    /**
+     * Initializes the controller class.
+     * @param url
+     * @param rb
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+    
+}
