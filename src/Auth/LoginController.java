e7a8ace6022c1e36d0b2dd1574d6bfd5cb4c74b5
TheOnlySmartBoy <o2jose43@gmail.com>
2019-06-03 20:05:58 +0300

    Complerted the login design

--- src/Auth/LoginController.java
+++ src/Auth/LoginController.java
@@ -2,13 +2,18 @@ package Auth;
 
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.AnchorPane;
 /**
  * FXML Controller class
  * @author Theonlysmartboy <Theonlysmartboy@github.com>
  */
 public class LoginController implements Initializable {
-
+@FXML 
+        
+       private AnchorPane parent;
     /**
      * Initializes the controller class.
      * @param url
@@ -18,5 +23,10 @@ public class LoginController implements Initializable {
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
     }    
-    
+
+    @FXML
+    private void closeApp(MouseEvent event) {
+        System.exit(0);
+    }
+   
 }
